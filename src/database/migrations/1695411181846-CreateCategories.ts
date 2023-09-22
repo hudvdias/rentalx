cac8721b02bf8e7859f3a0873c43f0fb963620a3
Hudson Dias <hudson.oliveira.dias@gmail.com>
2023-09-22 17:10:28 -0300

    criando migration de categoria

---
+++ src/database/migrations/1695411181846-CreateCategories.ts
@@ -0,0 +1,35 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCategories1695411181846 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const table = new Table({
+      name: "categories",
+      columns: [
+        {
+          name: "id",
+          type: "uuid",
+          isPrimary: true,
+        },
+        {
+          name: "name",
+          type: "varchar",
+        },
+        {
+          name: "description",
+          type: "varchar",
+        },
+        {
+          name: "created_at",
+          type: "timestamp",
+          default: "now()",
+        },
+      ],
+    });
+
+    await queryRunner.createTable(table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("categories");
+  }
+}
