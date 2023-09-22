3ddd68ce946a8601805a974d918102734d73ff3d
Hudson Dias <hudson.oliveira.dias@gmail.com>
2023-09-22 05:17:27 -0300

    inserindo tipagem para categoria

---
+++ src/models/category.ts
@@ -0,0 +1,13 @@
+import { v4 } from "uuid";
+
+export class Category {
+  id?: string;
+  name: string;
+  description: string;
+  created_at?: Date;
+
+  constructor() {
+    if (!this.id) this.id = v4();
+    if (!this.created_at) this.created_at = new Date();
+  }
+}
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
-import { v4 } from "uuid";
+import { Category } from "../models/category";
 
 export const categoriesRoutes = Router();
 
-const categories = [];
+const categories: Category[] = [];
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  const category = { id: v4(), name, description };
+  const category = new Category();
+  Object.assign(category, { name, description });
   categories.push(category);
   return response.status(201).json(category);
 });
